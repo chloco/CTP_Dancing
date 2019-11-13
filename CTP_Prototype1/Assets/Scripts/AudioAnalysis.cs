712726c30bef9d11216ce21e6453af6a326f4015
Chloe B <chloe5.brown@live.uwe.ac.uk>
2019-11-13 16:41:41 +0000

    detecting the hz and amplitute per frequency bin

--- CTP_Prototype1/Assets/Scripts/AudioAnalysis.cs
+++ CTP_Prototype1/Assets/Scripts/AudioAnalysis.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class AudioAnalysis : MonoBehaviour
 {
-
-
+    AudioSource audioSource;
 
     // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
 
+    }
+
+    void Update()
+    {
+            float[] curSpectrum = new float[1024];
+            audioSource.GetSpectrumData(curSpectrum, 0, FFTWindow.BlackmanHarris);
+
+            float targetFrequency = 234f;
+            float hertzPerBin = (float)AudioSettings.outputSampleRate / 2f / 1024;
+            int targetIndex = (int)(targetFrequency / hertzPerBin);
+
+            string outString = "";
+            for (int i = targetIndex - 3; i <= targetIndex + 3; i++)
+            {
+                outString += string.Format("| Bin {0} : {1}Hz : {2} |   ", i, i * hertzPerBin, curSpectrum[i]);
+            }
+
+            Debug.Log(outString);
+        
+    }
 }
