9e6c951bb84f81a924a7ac3873474b63d319ed11
Chloe Brown <chloe5.brown@live.uwe.ac.uk>
2020-04-19 21:45:44 +0100

    fairly accurate bpm now???

--- CTP_Prototype1/Assets/Scripts/Dance.cs
+++ CTP_Prototype1/Assets/Scripts/Dance.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RhythmTool;
+using System.Linq;
+using System;
+
 public class Dance : MonoBehaviour
 {
     public static string genre;
@@ -18,10 +21,13 @@ public class Dance : MonoBehaviour
     public static bool isPlaying = false;
 
     public RhythmAnalyzer analyzer;
-    public Beat
-    public RhythmData rhythmData;
+    //public Beat;
+    RhythmData rhythmData;
     private float prevTime;
     private List<Beat> beats;
+    BeatTracker beatTracker;
+    bool once;
+    Track<Beat> beatTrack;
 
     void Awake()
     {
@@ -33,7 +39,10 @@ public class Dance : MonoBehaviour
     {
         time = 0.5f;
         anim = GetComponent<Animator>();
-       source = GetComponent<AudioSource>();        
+       source = GetComponent<AudioSource>();
+        analyzer = GetComponent<RhythmAnalyzer>();
+        //rhythmData = GetComponent<RhythmData>();
+        beatTracker = GetComponent<BeatTracker>();
 
     }
 
@@ -53,117 +62,182 @@ public class Dance : MonoBehaviour
         {
             if (SetGenre.isSet)
             {
-             
-                Debug.Log("PLAY THE SONGS DUDE");
-          
+                isPlaying = true;
+                rhythmData = null;
                 rhythmData = analyzer.Analyze(source.clip);
-                
-            }
-            if(analyzer.initialized)
-            {
-                source.Play();
-                Track<Beat> track = rhythmData.GetTrack<Beat>();
-                //Track<Beat> track = RhythmTool.trackBeat();
-                SetGenre.isSet = false;
-            }
-            rhythmData.GetFeatures<Beat>(beats, prevTime, timeS);
 
-            private void OnBeat(Beat beat)
-            {
-                bpm = Mathf.Round(beat.bpm * 10) / 10;
+                Debug.Log(source.clip.name);
+                Debug.Log("PLAY THE SONGS DUDE");
+                source.Play();
+                SetGenre.isSet = false;
+
             }
-            foreach (Beat beat in beats)
-            {
-              
-                bpm = (int)beat.bpm;
-                anim.SetFloat("BPM", bpm);
-            }
+
+          
+            //rhythmData.GetFeatures<Beat>(beats, prevTime, timeS);
+
+            //private void OnBeat(Beat beat)
+            //{
+            //    bpm = Mathf.Round(beat.bpm * 10) / 10;
+            //}
+            //foreach (Beat beat in beats)
+            //{
+
+
+            //}
 
             if (Dance.isPlaying)
             {
-                anim.SetBool("MusicIsPlaying", true);
+                if (analyzer.initialized)
+                {
+                    beatTrack = rhythmData.GetTrack<Beat>();
 
-                time -= Time.fixedDeltaTime;
-                if(source.clip.name.Contains("Macarena") || source.clip.name.Contains("macarena"))
-                {
-                    anim.SetInteger("SpecialSongNum", 1);
-                }
-             
-                else if (time <= 0)
-                {
-                    while (DanceSelect == currentDance)
+                    //Group beats by rounded BPM
+                    Dictionary<int, List<Beat>> beatsByBPM = new Dictionary<int, List<Beat>>();
+
+                    for (int i = 0; i < beatTrack.count; i++)
                     {
-                        switch (Dance.genre)
+                        Beat beat = beatTrack[i];
+
+                        int bpm = Mathf.RoundToInt(beat.bpm);
+
+                        if (!beatsByBPM.ContainsKey(bpm))
+                            beatsByBPM.Add(bpm, new List<Beat>());
+
+                        beatsByBPM[bpm].Add(beat);
+                    }
+
+                    //Find group with most beats
+                    List<Beat> beats = beatsByBPM.Values.First();
+                    int count = beats.Count;
+
+                    foreach (var value in beatsByBPM.Values)
+                    {
+                        if (value.Count > count)
                         {
-                            case "Reggae":
-                                anim.SetInteger("Genre", (int)Genres.Reggae);
-                                Dance.DanceSelect = (int)Random.Range(0f, 16.0f);
-                                break;
-                            case "Hip-Hop":
-                                anim.SetInteger("Genre", (int)Genres.HipHop);
-                                Dance.DanceSelect = (int)Random.Range(0f, 33.0f);
-                                break;
-                            case "Pop":
-                                anim.SetInteger("Genre", (int)Genres.Pop);
-                                Dance.DanceSelect = (int)Random.Range(0f, 25.0f);
-                                break;
-                            case "RnB":
-                                anim.SetInteger("Genre", (int)Genres.RnB);
-                                Dance.DanceSelect = (int)Random.Range(0f, 28.0f);
-                                break;
-                            case "Techno":
-                                anim.SetInteger("Genre", (int)Genres.Techno);
-                                Dance.DanceSelect = (int)Random.Range(0f, 24.0f);
-                                break;
-                            case "Rock":
-                                anim.SetInteger("Genre", (int)Genres.Rock);
-                                Dance.DanceSelect = (int)Random.Range(0f, 17.0f);
-                                anim.SetFloat("BPM", bpm);
-                                break;
-                            case "Disco":
-                                anim.SetInteger("Genre", (int)Genres.Disco);
-                                Dance.DanceSelect = (int)Random.Range(0f, 19.0f);
-                                anim.SetFloat("BPM", bpm);
-                                break;
-                            case "Alternative":
-                                anim.SetInteger("Genre", (int)Genres.Alternative);
-                                Dance.DanceSelect = (int)Random.Range(0f, 22.0f);
-                                break;
-                            case "Rap":
-                                anim.SetInteger("Genre", (int)Genres.Rap);
-                                Dance.DanceSelect = (int)Random.Range(0f, 27.0f);
-                                //anim.SetFloat("BPM", bpm);
-                                break;
-                            case "80sPop":
-                                anim.SetInteger("Genre", (int)Genres.EightiesPop);
-                                Dance.DanceSelect = (int)Random.Range(0f, 9.0f);
-                                break;
+                            count = value.Count;
+                            beats = value;
                         }
                     }
 
-                    //time = 5;
-                    anim.SetInteger("DanceSelection", DanceSelect);
-                    currentDance = DanceSelect;
-                    float temp = anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndex("Dance")).length;
-                    //anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndex("Dance")).normalizedTime = 0.5f;
-                    time = Random.Range(0f, temp);
+                    //Find the average BPM
+                    float averageBPM = beats.Sum(b => b.bpm) / count;
+                    float averageBeatLength = 60 / averageBPM;
 
-                    Debug.Log("Change Moves" + DanceSelect);
-                    Debug.Log(time);
+                    //Find the most common offset rounded to x decimals
+                    Dictionary<float, int> offsetCount = new Dictionary<float, int>();
+                    int decimals = 3;
 
+                    foreach (var beat in beats)
+                    {
+                        float offset = (float)Math.Round(beat.timestamp % averageBeatLength, decimals);
+
+                        if (offsetCount.ContainsKey(offset))
+                            offsetCount[offset]++;
+                        else
+                            offsetCount.Add(offset, 1);
+                    }
+
+                    int n = 0;
+                    float bestOffset = 0;
+
+                    foreach (var item in offsetCount)
+                    {
+                        if (item.Value > n)
+                        {
+                            n = item.Value;
+                            bestOffset = item.Key;
+                        }
+                    }
+
+                    Debug.Log("Average BPM: " + averageBPM);
+                    Debug.Log("Best offset: " + bestOffset);
+                    bpm = (int)averageBPM;
+                    anim.SetFloat("BPM", bpm);
+                    anim.SetBool("MusicIsPlaying", true);
+
+                
+
+                    time -= Time.fixedDeltaTime;
+                    if (source.clip.name.Contains("Macarena") || source.clip.name.Contains("macarena"))
+                    {
+                        anim.SetInteger("SpecialSongNum", 1);
+                    }
+
+                    else if (time <= 0)
+                    {
+                        while (DanceSelect == currentDance)
+                        {
+                            switch (Dance.genre)
+                            {
+                                case "Reggae":
+                                    anim.SetInteger("Genre", (int)Genres.Reggae);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 16.0f);
+                                    break;
+                                case "Hip-Hop":
+                                    anim.SetInteger("Genre", (int)Genres.HipHop);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 33.0f);
+                                    break;
+                                case "Pop":
+                                    anim.SetInteger("Genre", (int)Genres.Pop);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 25.0f);
+                                    break;
+                                case "RnB":
+                                    anim.SetInteger("Genre", (int)Genres.RnB);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 28.0f);
+                                    break;
+                                case "Techno":
+                                    anim.SetInteger("Genre", (int)Genres.Techno);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 24.0f);
+                                    break;
+                                case "Rock":
+                                    anim.SetInteger("Genre", (int)Genres.Rock);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 17.0f);
+                                    anim.SetFloat("BPM", bpm);
+                                    break;
+                                case "Disco":
+                                    anim.SetInteger("Genre", (int)Genres.Disco);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 19.0f);
+                                    anim.SetFloat("BPM", bpm);
+                                    break;
+                                case "Alternative":
+                                    anim.SetInteger("Genre", (int)Genres.Alternative);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 22.0f);
+                                    break;
+                                case "Rap":
+                                    anim.SetInteger("Genre", (int)Genres.Rap);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 27.0f);
+                                    //anim.SetFloat("BPM", bpm);
+                                    break;
+                                case "80sPop":
+                                    anim.SetInteger("Genre", (int)Genres.EightiesPop);
+                                    Dance.DanceSelect = (int)UnityEngine.Random.Range(0f, 9.0f);
+                                    break;
+                            }
+                        }
+
+                        //time = 5;
+                        anim.SetInteger("DanceSelection", DanceSelect);
+                        currentDance = DanceSelect;
+                        float temp = anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndex("Dance")).length;
+                        //anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndex("Dance")).normalizedTime = 0.5f;
+                        time = UnityEngine.Random.Range(0f, temp);
+
+                        Debug.Log("Change Moves" + DanceSelect);
+                        Debug.Log(time);
+                    }
+                    }
+
+                else
+                {
+                    anim.SetBool("MusicIsPlaying", false);
+                    bpm = 0;
                 }
-
-
             }
-            else
-            {
-                anim.SetBool("MusicIsPlaying", false);
-                bpm = 0;
-            }
+
+            prevTime = timeS;
         }
-
-        prevTime = timeS;
-    }
+        }
 }
 
 public enum Genres
